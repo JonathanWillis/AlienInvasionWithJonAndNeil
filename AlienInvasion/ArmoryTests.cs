de86d039f2fc8f7142353e35b7f6b680500a279d
Jonathan Willis <jonwillis@gmail.com>
2013-07-04 07:38:24 +0100

    Seperating out classes to files

--- AlienInvasion/ArmoryTests.cs
+++ AlienInvasion/ArmoryTests.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using AlienInvasion.Client.AlienInvaders;
 using AlienInvasion.Client.DefenceAssets;
 using NUnit.Framework;
@@ -67,72 +66,6 @@ namespace AlienInvasion
         public void ThenTheSameWeaponUsedToCreateTheArmoryIsReturned()
         {
             Assert.That(_outputWeapon, Is.SameAs(_inputWeapon));
-        }
-    }
-
-    public class Peashooter500Blaster : IDefenceWeapon, IArmouryWeapon
-    {
-        private readonly IDefenceWeapon _defenceWeapon;
-        private int _reloadCounter;
-
-        public Peashooter500Blaster()
-        {
-            
-        }
-
-        public Peashooter500Blaster(IDefenceWeapon defenceWeapon)
-        {
-            _defenceWeapon = defenceWeapon;
-        }
-
-        public DefenceWeaponType DefenceWeaponType {  get { return DefenceWeaponType.Peashooter500Blaster; }}
-
-        public bool IsReloading()
-        {
-            return _reloadCounter > 0;
-        }
-
-        public void DeployTo(IList<IDefenceWeapon> weapons)
-        {
-            weapons.Add(_defenceWeapon);
-            _reloadCounter = 2;
-        }
-
-        public void NewWaveIncoming()
-        {
-            _reloadCounter--;
-        }
-    }
-
-    public class Peashooter1000Blaster : IDefenceWeapon, IArmouryWeapon
-    {
-        private readonly IDefenceWeapon _defenceWeapon;
-
-        public Peashooter1000Blaster()
-        {
-            
-        }
-
-        public Peashooter1000Blaster(IDefenceWeapon defenceWeapon)
-        {
-            _defenceWeapon = defenceWeapon;
-        }
-
-        public DefenceWeaponType DefenceWeaponType { get { return DefenceWeaponType.Peashooter1000Blaster; } }
-
-        public bool IsReloading()
-        {
-            return false;
-        }
-
-        public void DeployTo(IList<IDefenceWeapon> weapons)
-        {
-            weapons.Add(_defenceWeapon);
-        }
-
-        public void NewWaveIncoming()
-        {
-           
         }
     }
 }
--- AlienInvasion/EarthDefender.cs
+++ AlienInvasion/EarthDefender.cs
@@ -1,7 +1,5 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using AlienInvasion.Client;
-using AlienInvasion.Client.AlienInvaders;
 using AlienInvasion.Client.DefenceAssets;
 
 namespace AlienInvasion
@@ -28,71 +26,4 @@ namespace AlienInvasion
 		    return new DefenceStrategy(armoury.WeaponsFor(invasionWave.AlienInvaders));
 		}
 	}
-
-    public interface IArmouryWeapon
-    {
-        bool IsReloading();
-        void DeployTo(IList<IDefenceWeapon> weapons);
-        void NewWaveIncoming();
-    }
-
-    public class Armoury
-    {
-        private readonly List<IArmouryWeapon> _weaponsAvailableForDefence;
-
-        public Armoury()
-        {
-            _weaponsAvailableForDefence = new List<IArmouryWeapon>();
-        }
-
-        public IEnumerable<IDefenceWeapon> WeaponsFor(IAlienInvader[] alienInvaders)
-        {
-            var weaponsToFire = new List<IDefenceWeapon>();
-            var invaderCount = alienInvaders.Count();
-            NewWaveIncoming();
-            foreach (var weapon in _weaponsAvailableForDefence)
-            {
-                if (!weapon.IsReloading())
-                    weapon.DeployTo(weaponsToFire);
-               
-                if (invaderCount == weaponsToFire.Count)
-                    break;
-            }
-            return weaponsToFire;
-        }
-
-        private void NewWaveIncoming()
-        {
-            foreach (var weapon in _weaponsAvailableForDefence)
-            {
-                weapon.NewWaveIncoming();
-            }
-        }
-
-        public void Add(IArmouryWeapon weapon)
-        {
-            _weaponsAvailableForDefence.Add(weapon);
-        }
-    }
-
-    public class ArmouryBuilder
-    {
-        public static Armoury From(IEnumerable<IDefenceWeapon> weapons)
-        {
-            var armoury = new Armoury();
-            foreach (var defenceWeapon in weapons)
-            {
-                switch (defenceWeapon.DefenceWeaponType)
-                {
-                    case DefenceWeaponType.Peashooter500Blaster:
-                        armoury.Add(new Peashooter500Blaster(defenceWeapon));
-                        break;
-                    case DefenceWeaponType.Peashooter1000Blaster:
-                        armoury.Add(new Peashooter1000Blaster(defenceWeapon));
-                        break;
-                }
-            }
-            return armoury;
-        }
-    }
 }
